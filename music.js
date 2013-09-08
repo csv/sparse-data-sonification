41742885e1e6a9e4fb38b053095d772795228cf5
Thomas Levine <occurrence@thomaslevine.com>
2013-09-08 20:49:05 +0000

    give up on getting it working in firefox or node

--- music.js
+++ music.js
@@ -1,14 +1,10 @@
-var master = new webkitAudioContext();
-
+var master = new webkitAudioContext // require('web-audio-api').AudioContext()
 var jsynth = require('jsynth')
-  , tau = Math.PI * 2
-  , frequency = 555
-;
+var tau = Math.PI * 2
 
 var sineGenerator = function (time, index, input){
-  return Math.sin(time * tau * frequency)
+  return Math.sin(time * tau * 440)
 }
 
 var synth = jsynth(master, sineGenerator); // returns a web audio node
-
 synth.connect(master.destination)
