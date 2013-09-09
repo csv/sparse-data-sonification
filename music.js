8be32fe266df5a3c8a7ab75e2c416df0d9ed4c08
Thomas Levine <occurrence@thomaslevine.com>
2013-09-08 23:20:16 -0700

    oeu

--- music.js
+++ music.js
@@ -1,3 +1,4 @@
+/*
 var master = new (webkitAudioContext || AudioContext)
 var jsynth = require('jsynth')
 var tune = require('tune')
@@ -19,33 +20,22 @@ var synth = jsynth(master, function(t) {
   return tuned(t * 2)
 })
 synth.connect(master.destination)
+*/
 
-/*
 var master = new (webkitAudioContext || AudioContext) // require('web-audio-api').AudioContext()
 var jsynth = require('jsynth')
 var request = require('browser-request')
-var tune = require('tune')
-var teoria = require('teoria')
-
-var tones = []
-var notes = ['first', 'second', 'third', 'fourth', 'fifth']
-notes.map(function(i) {
-  var scale = teoria.note('a').scale('lydian')
-  var scaleNote = scale.get(i)
-  var newNote = teoria.note(scaleNote.toString())
-  var newScale = newNote.scale('lydian').simple()
-  newScale.map(function(n){ tones.push(n.toUpperCase()) })
-})
-
-console.log(tones)
-var tuned = tune(tones, {tempo: 8});
 
 function instrument(data,field) {
   return function (time, index, input){
     var i = index % data.length
-    var x = tuned(Math.round(data[i][field] * 1e8))
-    console.log(x)
-    return x
+    Math.round(data[i][field] * 1e7)
+/*
+    var freq = 110 * Math.pow(2, 3) // Math.round(Math.log(data[i][field] * 1e9)) - 4)
+    console.log(freq)
+*/
+  var freq = 880
+    return Math.sin(2 * Math.PI * time * freq)
   }
 }
 
@@ -55,4 +45,3 @@ request('ferry.smooth.json', function(err, res, body){
   var synth = jsynth(master, instrument(data,'Downtown.Passengers'))
   synth.connect(master.destination)
 })
-*/
