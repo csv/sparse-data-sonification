1be40f1492f5895265a5e1bf93d4960f961e3263
Thomas Levine <occurrence@thomaslevine.com>
2013-09-08 22:52:28 -0700

    oeu

--- music.js
+++ music.js
@@ -1,14 +1,51 @@
-var master = new webkitAudioContext // require('web-audio-api').AudioContext()
+var master = new (webkitAudioContext || AudioContext)
+var jsynth = require('jsynth')
+var tune = require('tune')
+var teoria = require('teoria')
+
+var tones = []
+var notes = ['first', 'second', 'third', 'fourth', 'fifth']
+notes.map(function(i) {
+  var scale = teoria.note('a').scale('lydian')
+  var scaleNote = scale.get(i)
+  var newNote = teoria.note(scaleNote.toString())
+  var newScale = newNote.scale('lydian').simple()
+  newScale.map(function(n){ tones.push(n.toUpperCase()) })
+})
+
+var tuned = tune(tones, {tempo: 8});
+
+var synth = jsynth(master, function(t) {
+  return tuned(t * 2)
+})
+synth.connect(master.destination)
+
+/*
+var master = new (webkitAudioContext || AudioContext) // require('web-audio-api').AudioContext()
 var jsynth = require('jsynth')
 var request = require('browser-request')
-var tau = Math.PI * 2
+var tune = require('tune')
+var teoria = require('teoria')
+
+var tones = []
+var notes = ['first', 'second', 'third', 'fourth', 'fifth']
+notes.map(function(i) {
+  var scale = teoria.note('a').scale('lydian')
+  var scaleNote = scale.get(i)
+  var newNote = teoria.note(scaleNote.toString())
+  var newScale = newNote.scale('lydian').simple()
+  newScale.map(function(n){ tones.push(n.toUpperCase()) })
+})
+
+console.log(tones)
+var tuned = tune(tones, {tempo: 8});
 
 function instrument(data,field) {
   return function (time, index, input){
     var i = index % data.length
-    // var freq = Math.pow(2, Math.round(data[i][field] * 1e4))
-    console.log(data[i][field] * 1e8)
-    return Math.sin(time * tau * freq)
+    var x = tuned(Math.round(data[i][field] * 1e8))
+    console.log(x)
+    return x
   }
 }
 
@@ -18,3 +55,4 @@ request('ferry.smooth.json', function(err, res, body){
   var synth = jsynth(master, instrument(data,'Downtown.Passengers'))
   synth.connect(master.destination)
 })
+*/
