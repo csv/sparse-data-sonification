d7dfd65331496d9ea9d6db921c29c275f658f07f
Thomas Levine <occurrence@thomaslevine.com>
2013-09-08 21:24:22 -0700

    generate the start of  the song

---
+++ ferry/ferry.r
@@ -0,0 +1,12 @@
+ferry <- read.csv('ferry.csv')
+ferry$time <- nrow(ferry):1
+
+
+k <- 30 * 1
+
+ferry.smooth <- data.frame(
+  Year = rep(ferry$Year, each = k),
+  Month = rep(ferry$Month, each = k),
+  Downtown.Passengers = density(ferry$Downtown.Passengers, n = k * nrow(ferry))$y,
+  Midtown.Passengers = density(ferry$Midtown.Passengers, n = k * nrow(ferry))$y
+)
